da8f7150b2d190edf38d46888f2c74376b05b1d3
George Kraft <cynerva@gmail.com>
2015-02-23 01:00:52 -0600

    Remove min-duration and max-duration lines from test.cljs
    They weren't actually doing anything. It's min-note-duration, not
    min-duration.

--- src/songstarters/test.cljs
+++ src/songstarters/test.cljs
@@ -8,12 +8,8 @@
 
 (go (let [
   duration (+ (rand 30) 30)
-  min-duration (- (Math/pow 2 (rand)) 0.9)
-  max-duration (+ min-duration (- (Math/pow 2 (rand 3)) 1))
   song (<! (random-song {
     :duration duration
-    :min-duration min-duration
-    :max-duration max-duration
   }))
   _ (do
     (.log js/console (pr-str song))
