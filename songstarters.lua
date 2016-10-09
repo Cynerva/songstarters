c67dfbf21218ea86f921dfec660a5189dd1204c4
George Kraft <cynerva@gmail.com>
2016-10-09 15:50:31 -0500

    add support for playing and recording beats

--- songstarters.lua
+++ songstarters.lua
@@ -14,9 +14,39 @@ local function addSound(path)
   table.insert(sounds, sound)
 end
 
-addSound("test.ogg")
-local format = formats.u32le
-local f = io.popen("ffplay -nodisp -f " .. format.text .. " -ac 2 -ar 44100 -i pipe:0", "w")
-for _,sample in ipairs(sounds[1]) do
-  format.write(sample, f)
+local function renderSongToStream(song, format, stream)
+  local text = song.text
+  local tickDuration = 44100 * 2 * 60 / song.tempo / song.subdivide
+  for i = 1,#text do
+    local c = text:sub(i, i)
+    local sound = sounds[tonumber(c) + 1]
+    for j = 1,tickDuration do
+      local sample = sound[j] or 0
+      format.write(sample, stream)
+    end
+  end
 end
+
+local function playSong(song)
+  local format = formats.u32le
+  local stream = io.popen("ffplay -nodisp -f " .. format.text .. " -ac 2 -ar 44100 -i pipe:0", "w")
+  renderSongToStream(song, format, stream)
+  stream:close()
+end
+
+local function renderSong(song)
+  local format = formats.u32le
+  local stream = io.popen("ffmpeg -f " .. format.text .. " -ac 2 -ar 44100 -i pipe:0 render.flac", "w")
+  renderSongToStream(song, format, stream)
+  stream:close()
+end
+
+addSound("kick.flac")
+addSound("hat.flac")
+addSound("snare.flac")
+
+renderSong({
+  text="0101222201012222",
+  tempo=120,
+  subdivide=2
+})
